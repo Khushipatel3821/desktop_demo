149bd08b617e88f4506bb710c828cb0f146b94c2
sojitrkh <Android@3821>
2025-04-22 15:20:57 +0530

    Convert chatting_chatters into a regular folder and add contents

---
+++ chatting_chatters/android/app/src/main/kotlin/com/sojitrkh/chatting_chatters/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sojitrkh.chatting_chatters
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ chatting_chatters/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:chatting_chatters/screens/chat_screen.dart';
+import 'package:chatting_chatters/screens/login_authentication_screen.dart';
+import 'package:chatting_chatters/screens/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting Chatters',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        useMaterial3: true,
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
+          return const LoginAuthenticationScreen();
+        },
+      ),
+    );
+  }
+}
